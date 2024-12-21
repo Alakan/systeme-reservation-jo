79e71245247a24ebfb5175662fa9710b1d342db9
Alakan <stricker.un@gmail.com>
2024-12-21 21:24:34 +0000

    Premier commit

---
+++ backend/systeme-reservation-jo/src/test/java/com/example/systeme_reservation_jo/SystemeReservationJoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.systeme_reservation_jo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemeReservationJoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
