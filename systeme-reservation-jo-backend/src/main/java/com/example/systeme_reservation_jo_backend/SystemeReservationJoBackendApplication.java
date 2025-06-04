0ce6a27eaf3e813f9b3c7832749ea58ed9264c5b
alakan <stricker.un@gmail.com>
2025-06-04 23:18:07 +0200

    Initial commit du monorepo avec la nouvelle structure backend/frontend

---
+++ systeme-reservation-jo-backend/src/main/java/com/example/systeme_reservation_jo_backend/SystemeReservationJoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.systeme_reservation_jo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemeReservationJoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemeReservationJoBackendApplication.class, args);
+	}
+
+}
---
+++ systeme-reservation-jo-backend/src/test/java/com/example/systeme_reservation_jo_backend/SystemeReservationJoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.systeme_reservation_jo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemeReservationJoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
