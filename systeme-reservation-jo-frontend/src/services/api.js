177ba043cb9f0e1c6885083163c0b60b35cd36af
alakan <stricker.un@gmail.com>
2025-06-05 22:54:54 +0200

    Adaptation déployement

--- systeme-reservation-jo-frontend/src/services/api.js
+++ systeme-reservation-jo-frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// Construit l'URL de base à partir de la variable d'environnement REACT_APP_API_URL,
+// ou utilise 'http://localhost:8080' si elle n'est pas définie.
+const baseURL = process.env.REACT_APP_API_URL ? `${process.env.REACT_APP_API_URL}/api` : 'http://localhost:8080/api';
+
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api', // Modifie cette URL selon ton environnement
+    baseURL: baseURL,
     headers: {
         'Content-Type': 'application/json',
     }
