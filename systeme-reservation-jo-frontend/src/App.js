6e44699a6fe7129009f1ef1cda66ae1fee0e2947
alakan <stricker.un@gmail.com>
2025-06-07 10:38:24 +0200

    Adaptation déployement

--- systeme-reservation-jo-frontend/src/App.js
+++ systeme-reservation-jo-frontend/src/App.js
@@ -1,11 +1,17 @@
 // src/App.js
 import { BrowserRouter as Router } from 'react-router-dom';
+import { useEffect } from 'react';
 import AppRouter from './router/AppRouter';
 import MenuRapide from './components/MenuRapide';
 import { UserProvider } from './contexts/UserContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 
 function App() {
+  // Ajout d'un useEffect pour loguer la valeur de REACT_APP_API_URL
+  useEffect(() => {
+    console.log("REACT_APP_API_URL:", process.env.REACT_APP_API_URL);
+  }, []);
+
   return (
     <UserProvider>
       <ThemeProvider>
