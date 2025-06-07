e59c0346e8773fb4f918356468629288f089e473
alakan <stricker.un@gmail.com>
2025-06-07 11:49:55 +0200

    Adaptation déployement

--- systeme-reservation-jo-frontend/src/services/api.js
+++ systeme-reservation-jo-frontend/src/services/api.js
@@ -1,12 +1,17 @@
+// src/services/api.js
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_URL ? `${process.env.REACT_APP_API_URL}/api` : 'http://localhost:8080/api';
+// La variable d'environnement est supposée contenir déjà le segment "/api"
+// Par exemple, sur Heroku : REACT_APP_API_URL=https://back-systeme-reservation-jo-a366aca13d32.herokuapp.com/api
+const baseURL = process.env.REACT_APP_API_URL
+  ? `${process.env.REACT_APP_API_URL}`
+  : 'http://localhost:8080/api';
 
 const api = axios.create({
-    baseURL: baseURL,
-    headers: {
-        'Content-Type': 'application/json',
-    }
+  baseURL: baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default api;
